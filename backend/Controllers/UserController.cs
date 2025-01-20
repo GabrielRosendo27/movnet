eec328f177d48bda34084cdfeb0612318033584d
GabrielRosendo27 <gabriel_eulalio@hotmail.com>
2025-01-20 16:23:58 -0300

    endpoint login

--- backend/Controllers/UserController.cs
+++ backend/Controllers/UserController.cs
@@ -20,13 +20,25 @@ public class UserController : ControllerBase
         var users = _context.Users.ToList();
         return Ok(users);
       }
-
       
       [HttpPost]
       public async Task<ActionResult<UserModel>> RegisterUser(UserModel user){
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
         return CreatedAtAction(nameof(SearchUsers), new {id = user.Id}, user);
+      }
+      [HttpPost("login")]
+      public ActionResult<string> Login(UserModel user){
+        if(string.IsNullOrEmpty(user.Email) || string.IsNullOrEmpty(user.Password)){
+          return BadRequest("Email ou senha são nulos.");
+        }
+        var foundUser = _context.Users.FirstOrDefault(u => u.Email == user.Email && u.Password == user.Password);
+
+        if(foundUser == null){
+          return Unauthorized("E-mail ou senha inválidos.");
+        }
+        return Ok(foundUser.User);
+
       }
   }
 }
