a1af43106398e52220497ca5acfc80f3aac1ed39
GabrielRosendo27 <gabriel_eulalio@hotmail.com>
2025-01-23 10:48:26 -0300

    frontend/register-buttons

--- frontend/netmov-front/src/components/register/Register.tsx
+++ frontend/netmov-front/src/components/register/Register.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { RegisterFormInputs } from "../validations/registerSchema";
 import { useRegisterForm } from "./hooks/useRegisterForm";
 import { useRegisterUser } from "./hooks/useRegisterUser";
+import { Menu } from "../menu/Menu";
 
 export function Register() {
   const navigate = useNavigate();
@@ -28,23 +29,26 @@ export function Register() {
   const { register, handleSubmit, errors } = useRegisterForm(onSubmit);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="bg-darkBlue w-screen h-screen flex items-center justify-center flex-col">
-        <div className="flex flex-col gap-1">
-          <Input text="Nome de usuário:" {...register("user")} type="text" />
-          {errors.user && <span className="text-red-600">{errors.user.message}</span>}
-          <Input text="Email:" {...register("email")} type="text" />
-          {errors.email && <span className="text-red-600">{errors.email.message}</span>}
-          <Input text="Senha:" {...register("password")} type="password" />
-          {errors.password && <span className="text-red-600">{errors.password.message}</span>}
+    <>
+      <Menu />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="bg-darkGradient w-screen h-screen flex items-center justify-center flex-col">
+          <div className="flex flex-col gap-1">
+            <Input text="Usuário" {...register("user")} type="text" />
+            {errors.user && <span className="text-red-600">{errors.user.message}</span>}
+            <Input text="Email" {...register("email")} type="text" />
+            {errors.email && <span className="text-red-600">{errors.email.message}</span>}
+            <Input text="Senha" {...register("password")} type="password" />
+            {errors.password && <span className="text-red-600">{errors.password.message}</span>}
+          </div>
+          <div className="flex gap-5">
+            <Button text="Voltar" onClick={onClick} className="bg-myPurple text-xl text-white px-8 py-4 hover:bg-indigo-800 mt-3" />
+            <Button text="Criar Conta" type="submit" className="bg-myPurple text-xl text-white px-8 py-4 hover:bg-indigo-800 mt-3" />
+          </div>
+          {isError && <span className="text-red-600">Erro ao registrar: {error?.message}</span>}
+          {isPending && <span className="text-green-400">Carregando...</span>}
         </div>
-        <div className="flex gap-5">
-          <Button text="Voltar" onClick={onClick} />
-          <Button text="Criar Conta" type="submit" />
-        </div>
-        {isError && <span className="text-red-600">Erro ao registrar: {error?.message}</span>}
-        {isPending && <span className="text-green-400">Carregando...</span>}
-      </div>
-    </form>
+      </form>
+    </>
   );
 }
