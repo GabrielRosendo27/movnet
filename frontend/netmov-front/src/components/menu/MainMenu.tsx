b4b49e9757cd7c2dcace4d5ff535d003ad43b99a
GabrielRosendo27 <gabriel_eulalio@hotmail.com>
2025-01-28 17:00:03 -0300

    frontend-main-input

--- frontend/netmov-front/src/components/menu/MainMenu.tsx
+++ frontend/netmov-front/src/components/menu/MainMenu.tsx
@@ -12,12 +12,12 @@ export function MainMenu() {
 
   const [showInput, setShowInput] = useState(false);
 
-  function handleAddMovie() {
-    setShowInput(true);
-    if (showInput === true) {
-      setShowInput(false);
-    }
-  }
+  // function handleAddMovie() {
+  //   setShowInput(true);
+  //   if (showInput === true) {
+  //     setShowInput(false);
+  //   }
+  // }
   return (
     <div className="p-4 bg-[#010C19] fixed top-0 left-0 w-full z-10">
       <ul className="flex gap-4 justify-between mx-12 items-center">
@@ -30,19 +30,19 @@ export function MainMenu() {
         </li>
         <li>
           <div className="relative">
-            <Button text="+ Adicionar Filme" className="px-4 py-2 text-sm bg-myPurple text-white hover:bg-indigo-800" onClick={handleAddMovie} />
-          </div>
-          {showInput ? (
-            <div className="flex items-center justify-center absolute right-[400px] bottom-[-50px] transition transition-duration-300">
-              <Input text="Filme" type="text" />
-              <Button text="Enviar" className="px-4 py-2 text-sm bg-myPurple text-white hover:bg-indigo-800 mb-5 ml-4" />
+            <Button text="+ Adicionar Filme" className="px-4 py-2 text-sm bg-myPurple text-white hover:bg-indigo-800" onClick={() => setShowInput(!showInput)} />
+
+            <div
+              className={`absolute left-[-50%] top-[110%] transition-all duration-500 ease-in-out  ${
+                showInput ? "opacity-100 pointer-events-auto z-0" : "opacity-0 pointer-events-none top-[94%]"
+              }`}
+            >
+              <div className="flex items-center justify-center ">
+                <Input text="" type="text" className={`rounded-sm bg-transparent`} />
+                <Button text="Adicionar" className="px-4 py-2 text-sm bg-myPurple text-white hover:bg-indigo-800 mb-5 ml-4" />
+              </div>
             </div>
-          ) : (
-            ""
-          )}
-          {/* <div className="absolute right-[400px] bottom-[-40px]">
-            <Input text="Filme" type="text" />
-          </div> */}
+          </div>
         </li>
         <li>
           <Button
