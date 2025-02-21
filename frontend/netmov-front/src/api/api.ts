8dba12a16ca45032169d30d77c4c53e55cb920ae
GabrielRosendo27 <gabriel_eulalio@hotmail.com>
2025-02-21 19:24:30 -0300

    fix: api-aws-endpoint-02

--- frontend/netmov-front/src/api/api.ts
+++ frontend/netmov-front/src/api/api.ts
@@ -1,18 +1,18 @@
 // const API_BASE_URL = import.meta.env.VITE_DEV_API_URL;
-const APP_URL = import.meta.env.REACT_APP_API_URL;
+const APP_URL = import.meta.env.VITE_PROD_API_URL;
 
 export const API_ENDPOINTS = {
   AUTH: {
-    LOGIN: `${APP_URL}/api/Auth/login`, // Login de usuário
-    LOGOUT: `${APP_URL}/api/Auth/logout`, // Saída de usuário
+    LOGIN: `${APP_URL}/Auth/login`, // Login de usuário
+    LOGOUT: `${APP_URL}/Auth/logout`, // Saída de usuário
   },
   MOVIES: {
-    GET_BY_NAME: (movieName: string) => `${APP_URL}/api/Movie/${encodeURIComponent(movieName)}`, // GET Título para buscar filme retornando filme
+    GET_BY_NAME: (movieName: string) => `${APP_URL}/Movie/${encodeURIComponent(movieName)}`, // GET Título para buscar filme retornando filme
   },
   USER: {
-    REGISTER: `${APP_URL}/api/User`,
-    GETUSERNAME: `${APP_URL}/api/User/get-username`,
-    MOVIES: `${APP_URL}/api/User/movies-list`,
-    MOVIEID: (movieId: number) => `${APP_URL}/api/User/movies/${movieId}`,
+    REGISTER: `${APP_URL}/User`,
+    GETUSERNAME: `${APP_URL}/User/get-username`,
+    MOVIES: `${APP_URL}/User/movies-list`,
+    MOVIEID: (movieId: number) => `${APP_URL}/User/movies/${movieId}`,
   },
 };
