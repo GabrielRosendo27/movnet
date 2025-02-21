53d7dd93618aa3d8f8a31344e094e3b5d21d4c61
GabrielRosendo27 <gabriel_eulalio@hotmail.com>
2025-02-21 16:19:27 -0300

    aws-08

--- backend/Program.cs
+++ backend/Program.cs
@@ -49,7 +49,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
     {
-        policy.WithOrigins("http://localhost:3000", "http://frontend","http://frontend:80", "http://localhost:5173", "http://15.228.127.149") 
+        policy.WithOrigins("http://localhost:3000", "http://frontend","http://frontend:80", "http://localhost:5173", "http://15.228.127.149", "https://movnet.tech", "http://movnet.tech") 
               .AllowAnyMethod()
               .AllowAnyHeader()
               .AllowCredentials()
